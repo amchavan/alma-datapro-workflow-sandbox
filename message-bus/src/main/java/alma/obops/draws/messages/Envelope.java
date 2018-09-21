5f80c8194d99431974baf3ac1e3859eed71212a6
amchavan <amchavan@eso.org>
2018-09-21 10:01:55 +0000

    Updated javadoc

--- message-bus/src/main/java/alma/obops/draws/messages/Envelope.java
+++ message-bus/src/main/java/alma/obops/draws/messages/Envelope.java
@@ -1,8 +1,10 @@
 package alma.obops.draws.messages;
 
+/** 
+ * An {@link Envelope} includes a {@link Message} providing metadata like forwarding information, timestamp, etc.
+ */
 public interface Envelope extends Record, Message {
 	public String getCreationTimestamp() ;
-
 	public Message getMessage();
 	public String getMessageClass();
 	public String getOriginIP();
