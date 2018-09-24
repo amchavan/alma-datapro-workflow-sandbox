9e15fcb0f0a690bef9c5a77303c75f85e8a51e64
Tomas Staig <tomas.staig@alma.cl>
2018-09-23 21:59:56 -0300

    Several changes in PLDriver.py file. Changed dictionaries to OrderedDict to guarantee order of execution of tasks. Changed internal methods and member variables to protected. Added support to pass parameters between tasks. Move some of the pipeline-driver.py script to PLDriver class. Moved (modified) main of pipeline-driver.py to PLDriver __main__ section. Added pipeline execution mostly copied from pipeline-driver.py.

--- workflow-db-mock/PLDriver.py
+++ workflow-db-mock/PLDriver.py
@@ -1,16 +1,35 @@
+import os
+import sys
+import time
 import json
 import inspect
+import argparse
+import tempfile
 import importlib
-import time
+import subprocess
+import collections
+sys.path.insert(0, "../shared")
+import dbdrwutils
+from dbmsgq import MqConnection, ExecutorClient
 
+import traceback
+
+#Interface for classes that wants to execute pre and post tasks.
 class Task():
     def __init__(self):
-        pass
-
-    def execute(self):
+        self._params = None
+    def execute(self, params):
         raise NotImplementedError
+    def getParams(self):
+        return self._params
+    def _check(self, params, cparams):
+        if not all(p in params for p in cparams):
+            raise TaskException("Params missing from '" + str(cparams) + "'. Current list is : '" + str(params.keys()) + "'")
+    def _checkNot(self, params, cparams):
+        if any(p in params for p in cparams):
+            raise TaskException("Params present from '" + str(cparams) + "'. Current list is : '" + str(params.keys()) + "'")
 
-
+#Generic exception for problems detected during tasks execution.
 class TaskException(Exception):
     def __init__(self, msg, ex=None):
         super().__init__(msg)
@@ -23,15 +42,23 @@ class TaskException(Exception):
         return "TaskException: " + self.__str__()
 
     def trace(self):
-        pass
+        #print(self.__traceback__)
+        if self.__traceback__ is not None:
+            traceback.print_tb(self.__traceback__)
 
-    def handle(ex):
+    def handle(ex, msg=None):
         if isinstance(ex, TaskException):
             return ex
+        elif msg is None:
+            ex2 = TaskException("Unhandled exception", ex)
+            ex2.__traceback__ = ex.__traceback__
+            return ex2
         else:
-            return TaskException("Unhandled exception", ex)
+            ex2 = TaskException(msg, ex)
+            ex2.__traceback__ = ex.__traceback__
+            return ex2
 
-
+#Class to accumulate stats of the execution of the tasks.
 class TaskStats():
     def __init__(self, name, task_type):
         self._name = name
@@ -93,35 +120,42 @@ class TaskStats():
             print("\tExecution: Not Done (0.0s)")
         if self._ex is not None:
             print("\t\t\tException: " + self._ex.msg())
+            self._ex.trace()
 
-
+#Generic class, which has the knowledge of the pre and post tasks to execute.
+#This information is loaded from a configuration file.
 class Template():
-    def __init__(self):
+    def __init__(self, params):
+        self._params = params
         #Read config file (from same dir)
         with open('config') as f:
-            self.config = json.load(f)
+            self._config = json.load(f, object_pairs_hook=collections.OrderedDict)
 
+    #Check that the configuration files is consistent and that it doesn't
+    #have duplicate entries.
     def check(self):
         res = True
         tmp = []
-        if 'pre-tasks' in self.config:
-            tmp += self.config['pre-tasks']
-        if 'post-tasks' in self.config:
-            tmp += self.config['post-tasks']
-        rep = {}
+        rep = collections.OrderedDict()
+        if 'pre-tasks' in self._config:
+            for t in self._config['pre-tasks']:
+                if t['name'] not in rep:
+                    rep[t['name']] = [t['name']]
+                rep[t['name']].append(t)
         rep['pipeline'] = ['pipeline', {'name':'pipeline'}]
-        for t in tmp:
-            if t['name'] not in rep:
-                rep[t['name']] = [t['name']]
-            rep[t['name']].append(t)
-            if len(rep[t['name']]) > 2:
+        if 'post-tasks' in self._config:
+            for t in self._config['post-tasks']:
+                if t['name'] not in rep:
+                    rep[t['name']] = [t['name']]
+                rep[t['name']].append(t)
+        for r in rep:
+            if len(rep[r]) > 2:
                 res = False
         return rep, res
 
-    #Dynamically load module and class.
-    #Check that it is actually a class.
+    #Dynamically load module and class. Check that it is actually a class.
     #Check that it inherits from 'Task' class.
-    def loadClass(self, mod, cl):
+    def _loadClass(self, mod, cl):
         c = None
         try:
             m = importlib.import_module(mod)
@@ -142,21 +176,21 @@ class Template():
 
     #Iterate over tasks of task_type retrieving the appropriate task sub-class.
     #Then execute the task itself.
-    def tasks(self, task_type, do=True):
+    def _tasks(self, task_type, do=True, cont=False):
         result = do
-        tstats = {}
-        if task_type not in self.config:
+        tstats = collections.OrderedDict()
+        if task_type not in self._config:
             return tstats, result
-        for t in self.config[task_type]:
+        for t in self._config[task_type]:
             ts = TaskStats(t['name'], task_type)
             tstats[t['name']] = ts
         if not do:
             return tstats, result
-        for t in self.config[task_type]:
+        for t in self._config[task_type]:
             ts = tstats[t['name']]
             try:
                 ts.start(['comp','load'])
-                cd = self.loadClass(t['module'], t['class'])
+                cd = self._loadClass(t['module'], t['class'])
                 ts.stop(['load'])
             except Exception as e:
                 ts.fail(['comp','load'])
@@ -164,11 +198,16 @@ class Template():
                 ex = TaskException.handle(e)
                 ts.setEx(ex)
                 result = False
-                continue
+                if cont:
+                    continue
+                else:
+                    break
             try:
                 ts.start(['exec'])
                 c = cd()
-                c.execute()
+                c.execute(self._params)
+                if c.getParams() is not None:
+                    self._params = c.getParams()
                 ts.stop(['comp','exec'])
             except Exception as e:
                 ts.fail(['comp','exec'])
@@ -176,44 +215,119 @@ class Template():
                 ex = TaskException.handle(e)
                 ts.setEx(ex)
                 result = False
-                continue
+                if cont:
+                    continue
+                else:
+                    break
             ts.finish(['comp','load','exec'])
         return tstats, result
 
-    def preTasks(self, do=True):
-        return self.tasks('pre-tasks', do)
+    def preTasks(self, do=True, cont=False):
+        return self._tasks('pre-tasks', do, cont)
 
-    def postTasks(self, do=True):
-        return self.tasks('post-tasks', do)
+    def postTasks(self, do=True, cont=False):
+        return self._tasks('post-tasks', do, cont)
 
-
+#Main class of the PLDriver implementation. It drives the execution
+#of the pre tasks, the pipeline itself and the post tasks.
 class PLDriver():
-    def __init__(self, prj, ous, recipe):
-        self.prj = prj
-        self.ous = ous
-        self.recipe = recipe
-        self.temp = Template()
+    def __init__(self, progID, ousUID, recipe):
+        self._progID = progID
+        self._ousUID = ousUID
+        self._recipe = recipe
+        #Initialization code for pipeline execution
+        # Make sure we know where we are
+        self.location = os.environ.get( 'DRAWS_LOCATION' )
+        if self.location == None:
+            raise RuntimeError( "DRAWS_LOCATION env variable is not defined" )
 
+        # Make sure we know where the local replicated cache directory is
+        self.replicatedCache = os.environ.get( 'DRAWS_LOCAL_CACHE' )
+        if self.replicatedCache == None:
+            raise RuntimeError( "DRAWS_LOCAL_CACHE env variable is not defined" )
+
+        self.pipelineScript = "pipeline.py"
+        self.thisDirectory = os.getcwd()		# Assume the pipeline script is in this same directory
+        self.pipelineExecutable = self.thisDirectory + "/" + self.pipelineScript
+        self.pipelineRunDirectory = tempfile.mkdtemp( prefix="drw-" )
+        self.workingDirectory = tempfile.mkdtemp( prefix="drw-" )
+        print( ">>> PipelineDriver: pipelineRunDirectory:", self.pipelineRunDirectory )
+        print( ">>> PipelineDriver: workingDirectory:", self.workingDirectory )
+        self.xtss = ExecutorClient( 'localhost', 'msgq', 'xtss' )
+        self.mq   = MqConnection( 'localhost', 'msgq' )
+
+        params = collections.OrderedDict()
+        params['progID'] = self._progID
+        params['ousUID'] = self._ousUID
+        params['recipe'] = self._recipe
+        params['location'] = self.location
+        params['pipelineRunDir'] = self.pipelineRunDirectory
+        params['replicatedCache'] = self.replicatedCache
+        self._temp = Template(params)
+
+    #In this method all the tasks are executed and are accounted for.
     def run(self):
         res = True
-        stats = {}
+        cont = True
+        stats = collections.OrderedDict()
         try:
-            rep, res = self.temp.check()
+            rep, res = self._temp.check()
+            res = res or cont
+            #print(rep)
             for r in rep:
                 if len(rep[r]) > 2:
-                    print(rep[r][0])
-            tstats, res = self.temp.preTasks(res)
+                    print(rep[r][0]+": "+str(len(rep[r])-1))
+            tstats, res = self._temp.preTasks(res, cont)
+            res = res or cont
             stats.update(tstats)
             ts = TaskStats('pipeline', 'pip')
             stats['pipeline'] = ts
-            if res:
-                pass
-                #res = pipeline()
-            (tstats, res) = self.temp.postTasks(res)
+            if res or cont:
+                res = self._pipeline(ts)
+            res = res or cont
+            (tstats, res) = self._temp.postTasks(res, cont)
+            res = res or cont
             stats.update(tstats)
         except Exception as e:
             res = False
             print("Exception handled")
             raise e
-        for ts in tstats:
-            tstats[ts].report()
+        for ts in stats:
+            stats[ts].report()
+
+    #Specific method to isolate the pipeline execution and error handling.
+    def _pipeline(self, ts):
+        res = True
+        try:
+            ts.start(['comp','exec'])
+            completed = subprocess.run( [self.pipelineExecutable, self._progID, self._ousUID, self._recipe], cwd=self.pipelineRunDirectory)
+            ret = completed.returncode
+            if ret != 0:
+                ts.fail(['comp','exec'])
+                res = False
+                print( ">>> PipelineDriver: Pipeline returned:", ret )
+                if ret != 2:
+                    raise RuntimeError( ">>> PipelineDriver: Pipeline returned: %d" % ret )
+                # Pipeline returned 2 -- processing failed
+                # Push the OUS to ProcessingProblem, and we're done
+                dbdrwutils.setState( self.xtss, self._ousUID, "ProcessingProblem" )
+        except Exception as e:
+            res = False
+            ts.fail(['comp','exec'])
+            ex = TaskException.handle(e)
+            ts.setEx(ex)
+        ts.stop(['comp','exec'])
+        return res
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pipeline Driver mock-up')
+    parser.add_argument(dest="progID", help="ID of the project containing the OUS")
+    parser.add_argument(dest="ousUID", help="ID of the OUS that should be processed")
+    parser.add_argument(dest="recipe", help="Pipeline recipe to run")
+    args=parser.parse_args()
+
+    print(">>> PipelineDriver: progID=%s, ousUID=%s, recipe=%s" % (args.progID, args.ousUID, args.recipe))
+
+    #Get from arguments
+    driver = PLDriver(args.progID, args.ousUID, args.recipe)
+    driver.run()
