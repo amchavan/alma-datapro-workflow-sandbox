60ebd117689c37f775211ea7253ed1f244d83a20
Tomas Staig <tomas.staig@alma.cl>
2019-01-09 12:54:54 -0300

    Added BasicSender and BasicReceiver Python implementation In the tests it could be seen that interaction with Java API worked transparently

---
+++ message-bus-demos/basic-sender/src/draws/examples/BasicSender.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+from draws.messages.Publisher import Publisher
+from draws.messages.rabbitmq.RabbitMqMessageBroker import RabbitMqMessageBroker
+
+from alma.obops.draws.examples.common.Person import Person
+
+class BasicSender:
+    #private Logger logger = LoggerFactory.getLogger( BasicReceiver.class );
+    def __init__(self):
+        self.__broker = RabbitMqMessageBroker("amqp://localhost:5672", "guest", "guest")
+    def run(self, args):
+        publisher = Publisher(self.__broker, args.qname)
+        freddie = Person("Freddie Mercury", 45, False)
+        print("Sending to " + args.qname)
+        envelope = publisher.publish(freddie)
+        print(">>> Sent to " + args.qname + ": " + str(envelope.getMessage()))
+        self.__broker.closeConnection()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--qname", "-q", help="Queue name to be used.", type=str, required=True)
+    args = parser.parse_args()
+    sender = BasicSender()
+    sender.run(args)
