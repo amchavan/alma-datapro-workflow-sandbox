5fd777e0d5ce15059582681809d9dfd93c1e2bc1
Tomas Staig <tomas.staig@alma.cl>
2019-01-11 05:00:31 -0300

    Added logic to Message generator to create file in given dir Path from the package is added to the given dir, to place the file in appropriate dirs for both Java and Python

--- message-bus/src/main/python/draws/messages/GenMessage.py
+++ message-bus/src/main/python/draws/messages/GenMessage.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 
@@ -8,14 +9,21 @@ def main(args):
     xslt = ET.parse(args.xslt)
     transform = ET.XSLT(xslt)
     newdom = transform(dom)
-    f = open(args.out, 'w')
-    f.write(str(newdom))
-    f.close()
+    path = dom.getroot().attrib['package'].replace('.','/')
+    if os.path.exists(args.dir) and os.path.isdir(args.dir):
+        path = args.dir + '/' + path
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path = path + '/' + args.out
+        f = open(path, 'w')
+        f.write(str(newdom))
+        f.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--xslt", "-x", help="XSLT transformation file.", type=str, required=True)
+    parser.add_argument("--xslt", "-x", help="XSLT transformation file path.", type=str, required=True)
     parser.add_argument("--in", "-i", help="XML to be transformed input file path.", dest="xml", type=str, required=True)
-    parser.add_argument("--out", "-o", help="Desired output file path.", type=str, required=True)
+    parser.add_argument("--out", "-o", help="Desired output file name.", type=str, required=True)
+    parser.add_argument("--dir", "-d", help="Desired output base directory path. If not given, '.' will be used", default='.', type=str, required=False)
     args = parser.parse_args()
     main(args)
