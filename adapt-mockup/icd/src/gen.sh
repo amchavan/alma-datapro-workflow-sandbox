d870973d8e3e73864eb43a846f3e03b2cb6ab109
Tomas Staig <tomas.staig@alma.cl>
2019-01-21 07:36:14 -0300

    Removed adapt-mockup/icd module since the defined messages will be part of adapt-messages module

--- adapt-mockup/icd/src/gen.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-for i in $(ls main/resources/*.xml --color=never |sed "s/.*\/\(.*\)\.xml/\1/"); do
-	echo python3 -m adapt.messagebus.GenMessage --java -i main/resources/$i.xml -o $i.java -d main/java
-	echo python3 -m adapt.messagebus.GenMessage --py -i main/resources/$i.xml -o $i.py -d main/python
-done
